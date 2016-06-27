1511da2f2ae3ad2d818dbbd4dd217fe919400a36
wyr8845 <wyr527572059@qq.com>
2016-06-27 13:01:40 +0800

    宣传图片切换js化

--- finally/js/script.js
+++ finally/js/script.js
@@ -1,3 +1,18 @@
+//根据页面更换宣传图ad
+function changeADPicture(num) {
+	var id = document.getElementById("ad");
+	
+	if (num == 1) {
+		id.src = "images/ad1.jpg";
+	} else if (num == 2) {
+		id.src = "images/ad2.jpg";
+	} else {
+		console.log("changeADPicture失败");
+	}
+}
+
+
+//根据iframe内的页面设置iframe的高度
 function setIframeHeight(iframe) {
 	if (iframe) {
 		var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
@@ -6,7 +21,6 @@ function setIframeHeight(iframe) {
 		}
 	}
 };
-
 window.onload = function() {
 	setIframeHeight(document.getElementById('external-frame'));
 };
