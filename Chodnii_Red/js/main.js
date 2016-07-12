6dae67adce9ecc03d3ccfa6f376ef35b8a3b3e28
wyr8845 <wyr527572059@qq.com>
2016-07-12 18:32:20 +0800

    个人首页初步完成

---
+++ Chodnii_Red/js/main.js
@@ -0,0 +1,12 @@
+//根据iframe内的页面设置iframe的高度
+function setIframeHeight(iframe) {
+	if (iframe) {
+		var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
+		if (iframeWin.document.body) {
+			iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
+		}
+	}
+};
+window.onload = function() {
+	setIframeHeight(document.getElementById('external-frame'));
+};
