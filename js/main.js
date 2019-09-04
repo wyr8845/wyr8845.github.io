100110ffa1a423340a656f47bfdb5999067f00a8
超镦逸 <wyr527572059@qq.com>
2019-09-04 21:52:33 +0800

    修改个人网站

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-//根据iframe内的页面设置iframe的高度
-function setIframeHeight(iframe) {
-	if(iframe) {
-		var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
-		if(iframeWin.document.body) {
-			iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
-		}
-	}
-};
-window.onload = function() {
-	setIframeHeight(document.getElementById('external-frame'));
-};
+$("#top").click(function() {
+	ChagePage("top.html");
+});
+
+$("#about").click(function() {
+	ChagePage("about.html");
+});
+
+$("#work").click(function() {
+	ChagePage("work.html");
+});
+
+$("#member").click(function() {
+	ChagePage("member.html");
+});
+
+$("#video").click(function() {
+	window.open("https://space.bilibili.com/8355981");
+});
+
+$("#contact").click(function() {
+	ChagePage("contact.html");
+});
+
+function ChagePage(pageName) {
+	$("#main").prop("src", pageName);
+}
---
+++ js/work.js
@@ -0,0 +1,31 @@
+$("#gotoy").click(function() {
+	GpPage("https://pan.baidu.com/s/1c02DBH2");
+});
+
+$("#gotoy2").click(function() {
+	GpPage("https://pan.baidu.com/s/1kT3dCxD ;");
+});
+
+$("#tank").click(function() {
+	GpPage("https://pan.baidu.com/s/1c2rBzm4");
+});
+
+$("#rainbow").click(function() {
+	GpPage("https://pan.baidu.com/s/1hs4yP3u");
+});
+
+$("#cube").click(function() {
+	GpPage("https://pan.baidu.com/s/1geHc5pD");
+});
+
+$("#empire").click(function() {
+	GpPage("https://store.steampowered.com/app/973390/EmpiresThe_Rise/");
+});
+
+$("#silver_tree").click(function() {
+	GpPage("https://store.steampowered.com/app/1069520/Green_Field_Silver_Tree/");
+});
+
+function GpPage(pageName) {
+	window.open(pageName);
+}
