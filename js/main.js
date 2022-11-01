f0e68065c8c748b095a16f2abe8c5697f0d4890b
超镦逸 <wyr527572059@qq.com>
2022-11-01 17:25:02 +0800

    2022修改
    删除多余内容
    网页适配
    独立游戏和公司游戏分离
    游戏展示动态生成

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
-$("#top").click(function() {
-	ChagePage("top.html");
-});
-
-$("#about").click(function() {
-	ChagePage("about.html");
-});
-
-$("#work").click(function() {
-	ChagePage("work.html");
-});
-
-$("#member").click(function() {
-	ChagePage("member.html");
-});
-
-$("#video").click(function() {
-	window.open("https://space.bilibili.com/8355981");
-});
-
-$("#contact").click(function() {
-	ChagePage("contact.html");
-});
-
-function ChagePage(pageName) {
-	$("#main").prop("src", pageName);
+function LoadJson() {
+	$.ajax({
+		type:"get",
+		url:"json/data.json",
+		async:true,
+		success:function(data) {
+			SetData(data);
+		}
+	});
 }
+
+function SetData(data) {
+	SetBox(data, "indie");
+	SetBox(data, "company");
+}
+
+function SetBox(data, id) {
+	var str ="";
+	
+	$.each(data[id], function(index, item) {
+		str += "<div class=\"box\" onclick=\"OpenPage('" + item["link"] + "')\">";
+		str += "<div><img src=\"" + item["image"] + "\" /></div>";
+		str += "<b>" + item["name"] + "</b>";
+		str += "<div>" + item["type"] + "</div>";
+		str += "</div>";
+	});
+	
+	$("#" + id).html(str);
+}
+
+function OpenPage(page) {
+	window.open(page);
+}
+
+LoadJson();
